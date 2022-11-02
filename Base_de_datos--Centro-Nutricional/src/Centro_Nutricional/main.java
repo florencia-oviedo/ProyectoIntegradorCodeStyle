26d9df6ace121d4941a5d44625526837bb1e2e83
Adriana Soledad Da Silva <adrianasilva523@gmail.com>
2022-11-01 21:34:28 -0300

    Creacion de Menu - parte 1

--- Base_de_datos--Centro-Nutricional/src/Centro_Nutricional/main.java
+++ Base_de_datos--Centro-Nutricional/src/Centro_Nutricional/main.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Centro_Nutricional;
 
-/**
- *
- * @author jp
- */
 public class main {
+    public static void main(String[] args) {
+        
+    }
     
+
+    public static void Menu (){
+        int i, j, opcion;
+        boolean bandera = false;
+        String datos_pac[][] = new String [100] [100];
+        int fila = 0;
+        int id = 10000;
+        datos_pac[0][0] = "Nombre y Apellido";
+        datos_pac[0][1] = "Altura";
+        datos_pac[0][2] = "Peso";
+        datos_pac[0][3] = "IMC";
+        datos_pac[0][4] = "ID";
+        datos_pac[0][5] = "s";
+        
+    }
 }
+
